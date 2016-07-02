383d681d3409f9c169e533949a428cbffbe7dded
Jeff Morin <jmorin722@gmail.com>
2016-07-01 22:45:54 -0400

    Add files via upload

---
+++ Assignment1/src/Transaction.java
@@ -0,0 +1,36 @@
+/**
+ * Jeff Morin
+ * Assignment 1
+ * 6/5/16
+ *
+ */
+
+public class Transaction {
+    protected String itemName;
+    protected double price;
+    protected int quantity;
+    protected double paymentAmnt;
+    protected enum Type { ORDER, PAYMENT }
+    private Type type;
+
+    public Transaction() { }
+    public Transaction(String name, double price,
+                       int quantity)
+    {
+        this.type = Type.ORDER;
+        this.itemName = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Transaction(double paymentAmnt) {
+        this.type = Type.PAYMENT;
+        this.paymentAmnt = paymentAmnt;
+    }
+
+    public double getPrice() { return price; }
+    public String getName() { return itemName; }
+    public int getQuantity() { return quantity; }
+    public Type getType() { return type; }
+    public double getPaymentAmnt() { return paymentAmnt; }
+}
