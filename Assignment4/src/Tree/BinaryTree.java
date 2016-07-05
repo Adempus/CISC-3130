a0e2c225691d1ebc13c32a84e9f2272a10a8a62b
Jeff Morin <jmorin722@gmail.com>
2016-07-05 01:28:58 -0400

    Update BinaryTree.java

--- Assignment4/src/Tree/BinaryTree.java
+++ Assignment4/src/Tree/BinaryTree.java
@@ -3,21 +3,32 @@ package Tree;
 /**
  * Jeff Morin
  * Assignment4
+ * CISC 3130
  * 6/26/16
+ *
+ */
+
+/** Creates a binary tree of comparable generic types. Integers, Doubles,
+ *  Floats, Booleans, Characters or any other comparable object can be used
+ *  to represent the tree's data. Bin tree data must be comparable and have
+ *  precedence.
  *
  */
 
 public class BinaryTree<T extends Comparable<T>>
 {
     private Node<T> root;
+
+    public BinaryTree() { }
     public BinaryTree(T rootData) {
         root = new Node<>(rootData);
     }
 
     @SafeVarargs
-    public final void insertValues(T ... data) {
-        for(T datum : data)
-            insert(root, datum);
+    // Accepts a variable number of data to insert into the tree.
+    public final void insert(T ... data) {
+        if(root == null) root = new Node<>(data[0]);
+        for(T datum : data) insert(root, datum);
     }
     protected void insert(Node<T> node, T datum) {
         if(node.datum.compareTo(datum) > 0) {
@@ -33,15 +44,18 @@ public class BinaryTree<T extends Comparable<T>>
         }
     }
 
-    public void deleteValue(T datum) {
-        root = delete(root, datum);
+    @SafeVarargs
+    // Accepts a variable number of data to remove from the tree.
+    public final Node<T> remove(T ... data) {
+        for(T datum : data) root = remove(root, datum);
+        return root;
     }
-    protected Node<T> delete(Node<T> node, T datum) {
+    protected Node<T> remove(Node<T> node, T datum) {
         if(node == null) return null;
         else if(datum.compareTo(node.datum) < 0)
-            node.leftChild = delete(node.leftChild, datum);
+            node.leftChild = remove(node.leftChild, datum);
         else if (datum.compareTo(node.datum) > 0)
-            node.rightChild = delete(node.rightChild, datum);
+            node.rightChild = remove(node.rightChild, datum);
         else {
             Node<T> tempNode = new Node<>(null);
             if(node.isLeaf())
@@ -53,23 +67,24 @@ public class BinaryTree<T extends Comparable<T>>
             else {
                 tempNode.datum = findMin(node.rightChild);
                 node.datum = tempNode.datum;
-                node.rightChild = delete(node.rightChild, tempNode.datum);
+                node.rightChild = remove(node.rightChild, tempNode.datum);
             }
         }
         return node;
     }
 
-    public int count() {
-        return count(root);
+    // Returns the number of nodes currently in the tree
+    public int size() {
+        return size(root);
     }
-    protected int count(Node<T> node) {
+    protected int size(Node<T> node) {
         if(node == null) return 0;
-        else return (1 + count(node.leftChild) +
-                count(node.rightChild));
+        else return (1 + size(node.leftChild) + size(node.rightChild));
     }
 
-    public Node<T> find(T item) {
-        return find(root, item);
+    // Searches for, and returns the node with the specified datum.
+    public Node<T> find(T datum) {
+        return find(root, datum);
     }
     protected Node<T> find(Node<T> subtree, T item) {
         if (subtree == null)
@@ -82,6 +97,7 @@ public class BinaryTree<T extends Comparable<T>>
             return find(subtree.rightChild, item);
     }
 
+    // returns the minimum value in the tree.
     public T min() { return findMin(root); }
     protected T findMin(Node<T> node) {
         if(node == null) return null;
@@ -90,6 +106,7 @@ public class BinaryTree<T extends Comparable<T>>
         return findMin(node.leftChild);
     }
 
+    //returns the maximum value in the tree.
     public T max() {
         return findMax(root);
     }
@@ -100,6 +117,7 @@ public class BinaryTree<T extends Comparable<T>>
         return findMax(node.rightChild);
     }
 
+    // Returns the number of children for a specified node.
     public int getChildren(T datum) {
         Node<T> foundNode = find(datum);
         if(!foundNode.isLeaf()) {
@@ -142,7 +160,12 @@ public class BinaryTree<T extends Comparable<T>>
         }
     }
 
+    // release all the nodes of the tree;
+    public void free() {
+        root = null;
+    }
+    // if there are no nodes, the tree is empty.
     public boolean isEmpty() {
-        return (count() == 0);
+        return (size() == 0);
     }
 }
