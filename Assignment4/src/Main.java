ef55c69149d774089f61bf6224eb1522f120ca9b
Jeff Morin <jmorin722@gmail.com>
2016-07-05 01:28:26 -0400

    Update Main.java

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -1,70 +1,131 @@
 /**
  * Jeff Morin
+ * Assignment 4
+ * CISC 3130
  * 6/26/16
  *
  */
 import Tree.BinaryTree;
-
 import java.io.File;
 import java.util.*;
 
 public class Main {
-    public static BinaryTree<Integer> intTree;
+    public static BinaryTree[] forrest;
     public static CSVModel treeDataModel;
 
-    public static void main(String[] args) {
-        System.out.print("Enter a path: ");
-        String path = new Scanner(System.in).nextLine();
-        treeDataModel = new CSVModel(new File(path));
-        Integer[] treeData = createIntTree(treeDataModel.getModel().get(0));
-        intTreeInsertion(treeData);
-        printTreeOrders();
-        printCount();
+    public static void main(String[] args)
+    {
+        System.out.print("Enter file path: ");
+        treeDataModel = new CSVModel(
+                new File(new Scanner(System.in).nextLine())
+        );
+
+        int i = 0;
+        forrest = new BinaryTree[100];
+        for(Object[] o : treeDataModel.getModel()) {
+            forrest[i] = createTree(parseData(o));
+            i++;
+        }
+        System.out.println(i + " sets of trees created from data.");
+        menu();
     }
 
-    // method to parse tree input data into an Integer array
-    public static Integer[] createIntTree(Object[] data) {
+    public static void menu() {
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("\nView data from which set? ");
+            int set = new Scanner(System.in).nextInt();
+            int operation;
+
+            optionLoop: do {
+                System.out.println("\nPerform an operation on set " + set + ": " +
+                        "\n1. Print orders\t 2. Insert Data" + "\n3. Delete Data\t 4. Count nodes"
+                        + "\n5. Get max\t\t 6. Get min" + "\n7. Free tree\t 8. Choose another set."
+                        + "\n9. Exit.");
+                operation = new Scanner(System.in).nextInt();
+                switch (operation) {
+                    case 1 : printTreeOrders(forrest[set]); break;
+                    case 2 : insertValue(forrest[set]); break;
+                    case 3 : deleteValue(forrest[set]); break;
+                    case 4 : printCount(forrest[set]); break;
+                    case 5 : System.out.println("Max: " + findMax(forrest[set])); break;
+                    case 6 : System.out.println("Min: " + findMin(forrest[set])); break;
+                    case 7 : freeTree(forrest[set]); break;
+                    case 9 : exit = true;
+                    default : break optionLoop;
+                }
+            } while(true);
+        }
+    }
+
+    // TODO: method to parse tree input data into an Integer array
+    public static Integer[] parseData(Object[] data) {
         Integer[] insertData = new Integer[data.length];
         for(int i = 0 ; i < data.length ; i++)
             insertData[i] = Integer.parseInt(data[i].toString());
         return insertData;
     }
 
+    public static BinaryTree<Integer> createTree(Integer[] data) {
+        BinaryTree<Integer> tree = new BinaryTree<>(data[0]);
+        tree.insert(data);
+        return tree;
+    }
+
     // TODO: method to insert the tree's initial values
-    public static void intTreeInsertion(Integer[] data) {
-        intTree = new BinaryTree<>(data[0]);
-        intTree.insertValues(data);
+    public static void insertValue(BinaryTree tree) {
+        System.out.print("Enter an insertion value: ");
+        int value = new Scanner(System.in).nextInt();
+        tree.insert(value);
+        System.out.println("Insert another value? [y/n]");
+        char choice = new Scanner(System.in).nextLine().charAt(0);
+        if(choice == 'y' || choice == 'Y') insertValue(tree);
     }
 
     // TODO: method to print the tree's orders
-    public static void printTreeOrders() {
-        System.out.println("\nPREORDER");
-        intTree.printPreOrder();
-        System.out.println("\nINORDER");
-        intTree.printInOrder();
-        System.out.println("\nPOSTORDER:");
-        intTree.printPostOrder();
+    public static void printTreeOrders(BinaryTree tree) {
+        if(!tree.isEmpty()) {
+            System.out.println("\nPREORDER");
+            tree.printPreOrder();
+            System.out.println("\nINORDER");
+            tree.printInOrder();
+            System.out.println("\nPOSTORDER:");
+            tree.printPostOrder();
+            System.out.println();
+        } else {
+            System.out.println("The tree is empty.");
+        }
     }
 
     // TODO: method to print the tree's counts
-    public static void printCount() {
-        System.out.printf("%nCount: %d", intTree.count());
+    public static void printCount(BinaryTree tree) {
+        System.out.printf("Size: %d%n", tree.size());
     }
 
-    // TODO: method to delete the tree's values.
-    public static void deleteValue() {
-        System.out.print("Enter a deletion value: ");
-        int deletionVal = new Scanner(System.in).nextInt();
-        intTree.deleteValue(deletionVal);
-        printTreeOrders();
-        printCount();
+    // TODO: method to remove the tree's values.
+    public static void deleteValue(BinaryTree<Integer> tree) {
+        System.out.println("Enter a deletion value: ");
+        int value = new Scanner(System.in).nextInt();
+        tree.remove(value);
+        System.out.println("remove another value? [y/n]");
+        char choice = new Scanner(System.in).nextLine().charAt(0);
+        if(choice == 'y' || choice == 'Y') deleteValue(tree);
     }
 
     // TODO: method to get the number of node's children
     public static void printChildren() {
-        System.out.println("How many children for which value? ");
-        int val = new Scanner(System.in).nextInt();
-        System.out.printf("%nChild count of node %d: %d",
-                val, intTree.getChildren(val));
+
+    }
+
+    public static void freeTree(BinaryTree tree) {
+        tree.free();
+    }
+
+    public static Comparable findMax(BinaryTree tree) {
+        return tree.max();
+    }
+
+    public static Comparable findMin(BinaryTree tree) {
+        return tree.min();
     }
 }
